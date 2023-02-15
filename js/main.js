f247d077280fd4fd72d7b7aadb0078df7d2bc387
leekyoungjoo <120783305+torijoolee@users.noreply.github.com>
2023-02-15 20:15:36 +0900

    js: scrollIntoView

--- js/main.js
+++ js/main.js
@@ -1,42 +1,17 @@
 (() => {
-  const sectionElems = document.getElementsByTagName("section");
-  const prevBtnElem = document.querySelector(".prevBtn");
-  const nextBtnElem = document.querySelector(".nextBtn");
-  let currentPage = 0;
-  const totalPage = sectionElems.length;
+  const navItemElems = document.querySelector(".nav-menu");
 
-  //스크롤이 움직여 나온 화면의 dataset을 출력한다
-  //현재 페이지 = dataset.
+  navItemElems.addEventListener("click", function (e) {
+    const target = e.target;
+    const link = target.dataset.link;
 
-  window.addEventListener("scroll", function () {
-    let step;
-  });
-
-  // when button clicked Scroll to  page up and down
-  for (i = 0; i < totalPage; i++) {
-    sectionElems[i] = i;
-    sectionElems[i].dataset.index = i;
-  }
-
-  nextBtnElem.addEventListener("click", function () {
-    if (currentPage >= 0) {
-      currentPage++;
-      handleScroll();
+    if (link == null) {
+      return;
     }
-  });
-  prevBtnElem.addEventListener("click", function () {
-    if (currentPage <= totalPage - 1 || currentPage > 0) {
-      currentPage--;
-      handleScroll();
-    }
-  });
-
-  function handleScroll() {
-    window.scrollTo({
-      top: sectionElems[currentPage].offsetTop,
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({
       behavior: "smooth",
     });
-    currentPage = sectionElems[currentPage].dataset.index;
-    console.log(currentPage);
-  }
+  });
 })();
