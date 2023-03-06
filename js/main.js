7d46fe776778d13ce49611cad9ece2114506f75b
leekyoungjoo <120783305+torijoolee@users.noreply.github.com>
2023-03-06 23:39:30 +0900

    CSS: button

--- js/main.js
+++ js/main.js
@@ -1,17 +1,1 @@
-(() => {
-  const navItemElems = document.querySelector(".nav-menu");
 
-  navItemElems.addEventListener("click", function (e) {
-    const target = e.target;
-    const link = target.dataset.link;
-
-    if (link == null) {
-      return;
-    }
-    console.log(event.target.dataset.link);
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({
-      behavior: "smooth",
-    });
-  });
-})();
