26fe5e0e493eb8df00448c0fc8035d577d40146a
leekyoungjoo <120783305+torijoolee@users.noreply.github.com>
2023-03-07 23:09:34 +0900

    progressbar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
+(() => {
+  // show "prevBtn" when scrolling down
+  const prevBtn = document.querySelector(".prevBtn");
+  const container = document.querySelector(".container");
 
+  prevBtn.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+
+  window.addEventListener("scroll", function () {
+    const barElem = document.querySelector(".progress-bar");
+
+    const maxScrollValue = document.body.offsetHeight - window.innerHeight;
+
+    const scrollPer = window.scrollY / maxScrollValue;
+    console.log(scrollPer);
+
+    barElem.style.width = scrollPer * 100 + "%";
+  });
+
+  // progress-bar
+
+  // window.addEventListener("scroll", function () {});
+})();
