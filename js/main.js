56cc63a6d324c13be28dc63ef5c8a5d10529a4db
leekyoungjoo <120783305+torijoolee@users.noreply.github.com>
2023-03-08 17:19:19 +0900

    final

--- js/main.js
+++ js/main.js
@@ -2,31 +2,38 @@
   // show "prevBtn" when scrolling down
   const prevBtn = document.querySelector(".prevBtn");
   const container = document.querySelector(".container");
+  let maxScrollValue;
 
   prevBtn.addEventListener("click", () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   });
+
+  function scrollValue() {
+    maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  }
+
   // progress-bar
   window.addEventListener("scroll", function () {
     const barElem = document.querySelector(".progress-bar");
-
-    const maxScrollValue = document.body.offsetHeight - window.innerHeight;
+    scrollValue();
+    console.log(maxScrollValue);
 
     const scrollPer = window.scrollY / maxScrollValue;
     // console.log(scrollPer);
 
     barElem.style.width = scrollPer * 100 + "%";
   });
+
   // scrollAnimation
-
   let item = document.querySelectorAll("section");
-
   let observer = new IntersectionObserver((e) => {
     e.forEach((item) => {
       if (item.isIntersecting) {
         item.target.style.opacity = 1;
+        item.target.style.transform = `translateY(0px)`;
       } else {
         item.target.style.opacity = 0;
+        item.target.style.transform = `translateY(50px)`;
       }
     });
   });
@@ -36,4 +43,7 @@
       observer.observe(item[i]);
     }
   });
+
+  // resize
+  window.addEventListener("resize", scrollValue);
 })();
