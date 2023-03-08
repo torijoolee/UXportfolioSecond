b37c4fefd5aa0dc4e85cb12166e4a51969aa0b3e
leekyoungjoo <120783305+torijoolee@users.noreply.github.com>
2023-03-08 16:17:22 +0900

    JS:observer

--- js/main.js
+++ js/main.js
@@ -6,19 +6,34 @@
   prevBtn.addEventListener("click", () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   });
-
+  // progress-bar
   window.addEventListener("scroll", function () {
     const barElem = document.querySelector(".progress-bar");
 
     const maxScrollValue = document.body.offsetHeight - window.innerHeight;
 
     const scrollPer = window.scrollY / maxScrollValue;
-    console.log(scrollPer);
+    // console.log(scrollPer);
 
     barElem.style.width = scrollPer * 100 + "%";
   });
+  // scrollAnimation
 
-  // progress-bar
+  let item = document.querySelectorAll("section");
 
-  // window.addEventListener("scroll", function () {});
+  let observer = new IntersectionObserver((e) => {
+    e.forEach((item) => {
+      if (item.isIntersecting) {
+        item.target.style.opacity = 1;
+      } else {
+        item.target.style.opacity = 0;
+      }
+    });
+  });
+
+  item.forEach((elem) => {
+    for (let i = 0; i < item.length; i++) {
+      observer.observe(item[i]);
+    }
+  });
 })();
